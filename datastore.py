1f51a6ba7bac0e1928fc175372afadd51e6969cf
Rajeevthomas <gollapothusubbu86@gmail.com>
2025-04-15 23:17:32 +0530

    clean-up

--- datastore.py
+++ datastore.py
@@ -1,17 +1,20 @@
-from google.cloud import firestore
+from google.cloud import datastore
 import uuid
 from datetime import datetime
 
-db = firestore.Client()
+datastore_client = datastore.Client()
+kind_name = "blood-cell-db"
 
 def create_blood_cell_record(user_name, email, image_path):
-    record_id = str(uuid.uuid4())
-    doc_ref = db.collection("blood-cell-db").document(record_id)
-    doc_ref.set({
-        "id": record_id,
-        "user_name": user_name,
-        "email": email,
-        "image_path": image_path,
-        "timestamp": datetime.utcnow()
-    })
-    return record_id
+     record_id = str(uuid.uuid4())
+     key = datastore_client.key(kind_name, record_id)
+     entity = datastore.Entity(key=key)
+     entity.update({
+         "id": record_id,
+         "user_name": user_name,
+         "email": email,
+         "image_path": image_path,
+         "timestamp": datetime.utcnow()
+     })
+     datastore_client.put(entity)
+     return record_id
