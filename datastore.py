f06e3e6af8b33901803d01c8eb13f6e285199416
Rajeevthomas <gollapothusubbu86@gmail.com>
2025-04-15 23:07:02 +0530

    Update datastore.py

--- datastore.py
+++ datastore.py
@@ -6,7 +6,7 @@ db = firestore.Client()
 
 def create_blood_cell_record(user_name, email, image_path):
     record_id = str(uuid.uuid4())
-    doc_ref = db.collection("Blood-cell-db").document(record_id)
+    doc_ref = db.collection("blood-cell-db").document(record_id)
     doc_ref.set({
         "id": record_id,
         "user_name": user_name,
